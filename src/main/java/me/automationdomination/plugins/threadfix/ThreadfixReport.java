bd648216c9ae66438e37819409623c3a61414da8
Brandon Spruth <Brandon.Spruth@morningstar.com>
2014-05-22 17:57:45 -0500

    Added ThreadfixReport, ThreadfixProjectAction, ThreadfixBuildAction, rest, and util packages. Removed ChartAction

---
+++ src/main/java/me/automationdomination/plugins/threadfix/ThreadfixReport.java
@@ -0,0 +1,11 @@
+package me.automationdomination.plugins.threadfix;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bspruth
+ * Date: 5/22/14
+ * Time: 5:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ThreadfixReport {
+}
---
+++ src/main/java/me/automationdomination/plugins/threadfix/ThreadfixResultsPublisher.java
@@ -0,0 +1,46 @@
+package me.automationdomination.plugins.threadfix;
+
+import hudson.tasks.BuildStepMonitor;
+import hudson.tasks.Recorder;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bspruth
+ * Date: 3/22/14
+ * Time: 12:05 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ThreadfixResultsPublisher extends Recorder {
+
+    private static final String DEFAULT_TOKEN = "x";
+    private String scan;
+    private Long appId;
+
+    // Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
+    //public static class DescriptorImpl extends BuildStepDescriptor<Publisher> {
+    @DataBoundConstructor
+    public ThreadfixResultsPublisher(String scan, String searchCondition, Long appId, String token) {
+
+        this.scan = scan;
+        this.appId = appId;
+
+    }
+
+    public String getScan() {
+        return scan;
+    }
+
+    public Long getAppId() {
+        return appId;
+    }
+
+    public String getToken() {
+        return DEFAULT_TOKEN;
+    }
+
+    public BuildStepMonitor getRequiredMonitorService() {
+        return BuildStepMonitor.NONE;
+    }
+
+}
