4277c251cb5423fc595eac0485a0ba0963795ad6
Brandon Spruth <brandon@spruth.fr>
2014-06-12 00:13:24 -0500

    added url validation to ApacheCommonsUrlValidator

--- src/main/java/me/automationdomination/plugins/threadfix/validation/ApacheCommonsUrlValidator.java
+++ src/main/java/me/automationdomination/plugins/threadfix/validation/ApacheCommonsUrlValidator.java
@@ -1,10 +1,12 @@
 package me.automationdomination.plugins.threadfix.validation;
 
 import org.apache.commons.validator.routines.UrlValidator;
+import me.automationdomination.plugins.threadfix.ThreadFixPublisher;
 
 public class ApacheCommonsUrlValidator implements ConfigurationValueValidator {
 
 	private final UrlValidator urlValidator = new UrlValidator();
+
 
 	@Override
 	public boolean isValid(final String value) {
@@ -12,7 +14,16 @@ public class ApacheCommonsUrlValidator implements ConfigurationValueValidator {
 			return false;
 
 		if (!urlValidator.isValid(value))
+            (value == null || value.length() == 0)
 			return false;
+
+        // TODO how do you access "url" from ThreadFixPublisher
+        if (urlValidator.isValid(url)) {
+            System.out.println(url + " is valid");
+        }
+        else {
+            System.out.println(url + " is invalid");
+        }
 
 		return true;
 	}
