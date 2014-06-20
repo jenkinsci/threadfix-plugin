8cea668f4093fcce18c2d65140dd78d9cb2bb43f
Christopher Baek (e13177) <christopher.baek@cmegroup.com>
2014-06-19 19:52:59 -0500

    remove command line service

--- src/main/java/me/automationdomination/service/commandline/CommandLineExecutionServiceException.java
+++
@@ -1,9 +0,0 @@
-package me.automationdomination.service.commandline;
-
-public class CommandLineExecutionServiceException extends Exception {
-
-	public CommandLineExecutionServiceException(final String message, final Throwable cause) {
-		super(message, cause);
-	}
-
-}
