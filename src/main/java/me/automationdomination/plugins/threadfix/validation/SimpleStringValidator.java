5cb65778395939d6a4467976fab22822d2a4e847
Brandon Spruth <brandon@automationdomination.me>
2014-07-02 12:34:47 -0500

    added token validation on doTokenCheck need to implement

--- src/main/java/me/automationdomination/plugins/threadfix/validation/SimpleStringValidator.java
+++ src/main/java/me/automationdomination/plugins/threadfix/validation/SimpleStringValidator.java
@@ -2,12 +2,19 @@ package me.automationdomination.plugins.threadfix.validation;
 
 public class SimpleStringValidator implements ConfigurationValueValidator {
 
+    private static final String TOKEN_PATTERN = "^(?=.*\\d)(?=.*[A-Za-z])[A-Za-z0-9]{44}$";
+
 	@Override
 	public boolean isValid(final String value) {
-		if (value == null || value.isEmpty()) 
+		if (value == null || value.isEmpty())
 			return false;
-			
-		return true;
-	}
+        return true;
+    }
+    // TODO: add test of 44 alphanumeric on string for token value
+    public boolean IsAlphaNumeric(final String value)
+    {
+        //if (RegexpMatcher matcher = RegexpUtils.getMatcher(TOKEN_PATTERN);
+        return true;
+    }
 
 }
