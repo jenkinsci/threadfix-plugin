529069f98698bcf63b9aed540ad35d4238c9cea5
Brandon Spruth <brandon@automationdomination.me>
2014-08-10 23:58:21 -0500

    edited WindowsEnvironmentVariableParsingService for debugging added log var

--- src/main/java/me/automationdomination/plugins/threadfix/service/EnvironmentVariableParsingService.java
+++ src/main/java/me/automationdomination/plugins/threadfix/service/EnvironmentVariableParsingService.java
@@ -1,9 +1,10 @@
 package me.automationdomination.plugins.threadfix.service;
 
+
 import hudson.EnvVars;
+import java.io.PrintStream;
 
-public interface EnvironmentVariableParsingService {
-
-	public String parseEnvironentVariables(EnvVars envVars, String value);
-
+public abstract interface EnvironmentVariableParsingService
+{
+    public abstract String parseEnvironentVariables(EnvVars paramEnvVars, String paramString, PrintStream paramPrintStream);
 }
--- src/main/java/me/automationdomination/plugins/threadfix/service/WindowsEnvironmentVariableParsingService.java
+++ src/main/java/me/automationdomination/plugins/threadfix/service/WindowsEnvironmentVariableParsingService.java
@@ -2,43 +2,44 @@ package me.automationdomination.plugins.threadfix.service;
 
 import hudson.EnvVars;
 import java.io.PrintStream;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WindowsEnvironmentVariableParsingService implements EnvironmentVariableParsingService {
-	
+
 	private final Pattern environmentVariablePattern = Pattern.compile("%.+?%");
 
 	@Override
 	public String parseEnvironentVariables(final EnvVars envVars, final String value, PrintStream log) {
-		final Matcher matcher = environmentVariablePattern.matcher(value);
-		
+		final Matcher matcher = this.environmentVariablePattern.matcher(value);
+
 		String parsedValue = value;
-		
+
 		while (matcher.find()) {
 			final String matchedValue = matcher.group();
 			log.println("matchedValue: " + matchedValue);
-			
+
 			// TODO: can this be done more efficiently?
 			final String environmentVariableKey = matchedValue.replaceAll("%", "");
 			//
 			log.println("environmentVariableKey: " + environmentVariableKey);
-	
+
 			String environmentVariableValue = envVars.get(environmentVariableKey);
 			//
 			log.println("environmentVariableValue: " + environmentVariableValue);
-	
+
 			// if this is null, that means the environment variable was not found
 			if (environmentVariableValue != null) {
 				environmentVariableValue.replaceAll("\\\\", "\\\\\\\\");
-				
+
 				// TODO: can this be done more efficiently?
 				parsedValue = parsedValue.replaceAll("%" + environmentVariableKey + "%", environmentVariableValue);
-				// 
+				//
 				log.println("parsedValue: " + parsedValue);
 			}
 		}
-		
+
 		return parsedValue;
 	}
 
