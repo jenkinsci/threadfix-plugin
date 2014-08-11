660a21841f68974244acf2ca8afa0c053f3a3a73
Brandon Spruth <brandon@automationdomination.me>
2014-08-10 23:31:55 -0500

    edited WindowsEnvironmentVariableParsingService for debugging

--- src/main/java/me/automationdomination/plugins/threadfix/service/WindowsEnvironmentVariableParsingService.java
+++ src/main/java/me/automationdomination/plugins/threadfix/service/WindowsEnvironmentVariableParsingService.java
@@ -1,7 +1,7 @@
 package me.automationdomination.plugins.threadfix.service;
 
 import hudson.EnvVars;
-
+import java.io.PrintStream;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,18 +17,25 @@ public class WindowsEnvironmentVariableParsingService implements EnvironmentVari
 		
 		while (matcher.find()) {
 			final String matchedValue = matcher.group();
+			log.println("matchedValue: " + matchedValue);
 			
 			// TODO: can this be done more efficiently?
 			final String environmentVariableKey = matchedValue.replaceAll("%", "");
-			
+			//
+			log.println("environmentVariableKey: " + environmentVariableKey);
+	
 			String environmentVariableValue = envVars.get(environmentVariableKey);
-			
+			//
+			log.println("environmentVariableValue: " + environmentVariableValue);
+	
 			// if this is null, that means the environment variable was not found
 			if (environmentVariableValue != null) {
 				environmentVariableValue.replaceAll("\\\\", "\\\\\\\\");
 				
 				// TODO: can this be done more efficiently?
 				parsedValue = parsedValue.replaceAll("%" + environmentVariableKey + "%", environmentVariableValue);
+				// 
+				log.println("parsedValue: " + parsedValue);
 			}
 		}
 		
