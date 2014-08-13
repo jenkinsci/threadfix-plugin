ae9da967fc32dc02287c08288ee2e2615de6fdcb
Brandon Spruth <brandon@automationdomination.me>
2014-08-12 19:37:47 -0500

    added extra whacks on the WindowsEnvironmentVariableParsing

--- src/main/java/me/automationdomination/plugins/threadfix/ThreadFixPublisher.java
+++ src/main/java/me/automationdomination/plugins/threadfix/ThreadFixPublisher.java
@@ -71,7 +71,7 @@ public class ThreadFixPublisher extends Recorder {
 		final EnvironmentVariableParsingService environmentVariableParsingService;
 		
 		final String operatingSystem = System.getProperty("os.name");
-		
+
 		if (operatingSystem.toLowerCase().startsWith("windows")) {
 			log.println("detected windows os");
 			
